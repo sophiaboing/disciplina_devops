3a5b00d62d4ea6b3350aadfac5471c18882c258a
Sophia Boing Righetto <spr@certi.org.br>
2023-04-23 20:12:22 -0300

    add no src

---
+++ src/complexoperations/JustSomeMath.py
@@ -0,0 +1,23 @@
+import math
+import random
+
+
+class JustSomeMath:
+    """
+    Provides `nonsense` math operations for educational purposes only
+    """
+
+    def __init__(self) -> None:
+        """Math operations init"""
+        pass
+
+    def operate(self, number: int = 0) -> int:
+        '''
+        Does an exponentiation operation on the passed number
+        using a random exponend
+
+        :param int number: The number for the complex operation
+        :return int: the operation result
+        '''
+        random_int = random.randint(0, number)
+        return math.pow(number, random_int)
